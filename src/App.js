67c8088722983482793997c7beec5e0e9cfb2c02
Pannu786 <thepannu786@gmail.com>
2022-03-24 20:56:39 +0530

    added routes+components+container

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import { Login } from '../components/Login';
+import { Home } from '../container/Home';
+
 const App = () => {
   return (
-    <h1 className='text-3xl font-bold underline text-sky-400'>Hello world!</h1>
+    <Routes>
+      <Route path='login' element={<Login />} />
+      <Route path='/*' element={<Home />} />
+    </Routes>
   );
 };
 
