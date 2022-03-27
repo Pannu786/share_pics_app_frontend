37c16e567f9cc76f75d20405820dc6bb67abda92
Pannu786 <thepannu786@gmail.com>
2022-03-27 21:37:43 +0530

    added togglesidebar+set basic navbar in Home cmp

--- src/container/Home.js
+++ src/container/Home.js
@@ -7,13 +7,13 @@ import logo from '../assets/Share-Pics-logos_black.png';
 
 import userQuery from '../utils/data';
 import { Sidebar, UserProfile } from '../components';
-import Pins from './Pins';
 import { client } from '../client';
+import Pins from './Pins';
 
 const Home = () => {
   const [toggleSidebar, setToggleSidebar] = useState(false);
   const [user, setUser] = useState(null);
-
+  const scrollRef = useRef(null);
   //* this will fetch the data of user from localStorage (Login file) ---
   const userInfo =
     localStorage.getItem('user') !== 'undefined'
@@ -28,26 +28,31 @@ const Home = () => {
     });
   }, []);
 
+  useEffect(() => {
+    scrollRef.current.scrollTo(0, 0);
+  }, []);
+
   return (
     <div className='flex bg-gray-50 md:flex-row flex-col h-screen transaction-height duration-75 ease-out'>
       <div className='hidden md:flex h-screen flex-initial'>
         <Sidebar user={user && user} />
       </div>
       <div className='flex md:hidden flex-row'>
-        <HiMenu
-          fontSize={35}
-          className='cursor-pointer'
-          onClick={() => setToggleSidebar(true)}
-        />
-        <Link to='/'>
-          <img src={logo} width='145px' alt='logo' />
-        </Link>
+        <div className='p-2 w-full flex flex-row justify-between items-center shadow-md'>
+          <HiMenu
+            fontSize={35}
+            className='cursor-pointer'
+            onClick={() => setToggleSidebar(true)}
+          />
+          <Link to='/'>
+            <img src={logo} width='145px' alt='logo' />
+          </Link>
 
-        {/* //*this will get data of user from localStorage ---  */}
-        <Link to={`user-profile/${user?._id}`}>
-          <img src={user?.image} width='145px' alt='logo' />
-        </Link>
-      </div>
+          {/* //*this will get data of user from localStorage ---  */}
+          <Link to={`user-profile/${user?._id}`}>
+            <img src={user?.image} width='145px' alt='logo' />
+          </Link>
+        </div>
       {toggleSidebar && (
         <div className='fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in'>
           <div className='absolute w-full flex justify-end item-center p-2'>
@@ -59,7 +64,15 @@ const Home = () => {
           </div>
           <Sidebar user={user && user} closeToggle={setToggleSidebar} />
         </div>
-      )}
+        )}
+        
+      </div>
+      <div className='pb-2 flex-1 h-screen overflow-y-scroll' ref={scrollRef}>
+        <Routes>
+          <Route path='/user-profile/:userId' element={<UserProfile />} />
+          <Route path='/*' element={<Pins user={user && user} />} />
+        </Routes>
+      </div>
     </div>
   );
 };
