492b82d23b710aa842fabadf7eb40f678154178c
Pannu786 <thepannu786@gmail.com>
2022-03-28 22:26:09 +0530

    Added basic stuff to Sidebar cpm

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,3 +1,10 @@
+import React from 'react';
+import { NavLink, Link } from 'react-router-dom';
+import { RiHomeFill } from 'react-icons/ri';
+import { IoIOsArrowForward } from 'react-icons/io';
+
+import logo from '../assets/Share-Pics-logos_black.png';
+
 const Sidebar = () => {
   return <div>Sidebar</div>;
 };
