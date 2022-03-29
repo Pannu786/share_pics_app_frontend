874f4fd8fc7eee60d63bdc388cf8d9eb79402fe3
Pannu786 <thepannu786@gmail.com>
2022-03-29 22:30:48 +0530

    added basic type categories in sidebar cmp

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -5,8 +5,71 @@ import { IoIOsArrowForward } from 'react-icons/io';
 
 import logo from '../assets/Share-Pics-logos_black.png';
 
-const Sidebar = () => {
-  return <div>Sidebar</div>;
+const isNotActiveStyle =
+  'flex items-center px-5 gap-3 text-gray-500 hover:text-black transition-all duration-200 ease-in-out capitalize ';
+
+const isActiveStyle =
+  'flex items-center px-5 gap-3 font-extrabold border-r-2 border-black  transition-all duration-200 ease-in-out capitalize ';
+
+//*Mock array of categories ---
+const categories = [
+  { name: 'Animals' },
+  { name: 'Wallpapers' },
+  { name: 'Photography' },
+  { name: 'Gaming' },
+  { name: 'Coding' },
+  { name: 'Other' },
+];
+
+const Sidebar = ({ user, closeToggle }) => {
+  const handleCloseSidebar = () => {
+    if (closeToggle) closeToggle(false);
+  };
+
+  return (
+    <div className='flex flex-col justify-between bg-white h-full overflow-y-scroll min-w-210 hide-scrollbar'>
+      <div className='flex flex-col'>
+        <Link
+          to='/'
+          className='flex px-5 my-6 pt-1 w-190 items-center'
+          onClick={handleCloseSidebar}
+        >
+          <img src={logo} alt='logo' className='w-full' />
+        </Link>
+        <div className='flex flex-col gap-5'>
+          <NavLink
+            to='/'
+            //* isActive(pro) comes with NavLink it self ---
+            className={({ isActive }) =>
+              isActive ? isActiveStyle : isNotActiveStyle
+            }
+            onClick={handleCloseSidebar}
+          >
+            <RiHomeFill />
+            Home
+          </NavLink>
+          <h3 className='mt-2 px-5 text-base 2xl:text-xl'>
+            Discover Categories
+          </h3>
+          {/* Here we are using map function to loop through the categories
+          array and return the categories, we are using slice method -1 to get
+          ride of "Other" Category --- */}
+          {categories.slice(0, categories.length - 1).map((category) => (
+            <NavLink
+              to={`/category/${category.name}`}
+              className={({ isActive }) =>
+                isActive ? isActiveStyle : isNotActiveStyle
+              }
+              onClick={handleCloseSidebar}
+              key={category.name}
+            >
+              {category.name }
+            </NavLink>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
