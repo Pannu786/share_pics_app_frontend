bc762ff51bd595857ca6b84858292184e7ec8cff
Pannu786 <thepannu786@gmail.com>
2022-04-03 23:13:08 +0530

    added download btn and icon on Pin cmp

--- src/components/Pin.js
+++ src/components/Pin.js
@@ -1,100 +1,47 @@
-import { Link, useNavigate } from 'react-router-dom';
 import { useState } from 'react';
-
+import { Link, useNavigate } from 'react-router-dom';
 import { v4 as uuidv4 } from 'uuid';
 import { MdDownloadForOffline } from 'react-icons/md';
 import { AiTwotoneDelete } from 'react-icons/ai';
 import { BsFillArrowUpRightCircleFill } from 'react-icons/bs';
 
 import { client, urlFor } from '../client';
-import { fetchUser } from '../utils/fetchUser';
 
 const Pin = ({ pin: { image, postedBy, _id, destination, save } }) => {
   const [postHovered, setPostHovered] = useState(false);
   const [savingPost, setSavingPost] = useState(false);
 
   const navigate = useNavigate();
-  const user = fetchUser();
-
-  const alreadySaved = !!save?.filter(
-    (item) => item.postedBy._id === user.googleId
-  )?.length;
-
-  const savePin = (id) => {
-    if (!alreadySaved) {
-      setSavingPost(true);
-
-      client
-        .patch(id)
-        .setIfMissing({ save: [] })
-        .insert('after', 'save[-1]', [
-          {
-            _key: uuidv4(),
-            userId: user.googleId,
-            postedBy: {
-              _type: 'postedBy',
-              _ref: user.googleId,
-            },
-          },
-        ])
-        .commit()
-        .then(() => {
-          window.location.reload();
-          setSavingPost(false);
-        });
-    }
-  };
 
   return (
-    <div className='m-2'>
+    <div className='m-2 '>
       <div
-        className='relative cursor-zoom-in w-auto hover:shadow-lg rounded-lg overflow-hidden transition-all duration-500 ease-in-out'
         onMouseEnter={() => setPostHovered(true)}
         onMouseLeave={() => setPostHovered(false)}
         onClick={() => navigate(`/pin-detail/${_id}`)}
+        className='relative cursor-zoom-in w-auto hover-shadow-lg rounded-lg overflow-hidden transition-all duration-500 ease-in-out transform hover:scale-105'
       >
         <img
-          className='rounded-lg w-full h-full flex flex-col justify-between p-1 pr-2 pt-2 pb-2 z-50'
+          className='rounded-lg w-full'
           alt='user-post'
           src={urlFor(image).width(250).url()}
         />
         {postHovered && (
           <div
-            className='absolute top-0 w-full h-full'
+            className='absolute top-0 w-full h-full flex flex-col justify-between p-1 pr-2 pt-2 pb-2 z-50'
             style={{ height: '100%' }}
           >
-            <div className='flex items-center justify-between'>
+            <div className='flex items-center justify-between '>
               <div className='flex gap-2'>
                 <a
-                  className='bg-white w-9 h-9 rounded-full flex items-center justify-center text-dark text-xl opacity-75 hover:opacity-100 hover:shadow-md outline-none         '
                   href={`${image?.asset?.url}?dl=`}
                   download
-                  onClick={(e) => {
-                    e.stopPropagation();
-                  }}
+                  onClick={(e) => e.stopPropagation()}
+                  className='bg-white w-9 h-9 rounded-full flex items-center justify-center text-dark text-xl opacity-75 hover:opacity-100'
                 >
                   <MdDownloadForOffline />
                 </a>
               </div>
-              {alreadySaved ? (
-                <button
-                  type='button'
-                  className='bg-red-500 opacity-70 hover:opacity-100 text-white font-bold py-1 px-5 text-base rounded-3xl hover:shadow-md outlined-none '
-                >
-                  {save?.length} Saved
-                </button>
-              ) : (
-                <button
-                  onClick={(e) => {
-                    e.stopPropagation();
-                    savePin(_id);
-                  }}
-                  type='button'
-                  className='bg-red-500 opacity-70 hover:opacity-100 text-white font-bold py-1 px-5 text-base rounded-3xl hover:shadow-md outlined-none '
-                >
-                  Save
-                </button>
-              )}
             </div>
           </div>
         )}
@@ -102,5 +49,4 @@ const Pin = ({ pin: { image, postedBy, _id, destination, save } }) => {
     </div>
   );
 };
-
 export default Pin;
