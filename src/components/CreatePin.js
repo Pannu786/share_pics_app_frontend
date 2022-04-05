151715acba8b764865a885b8d2d5c0fc85e56c49
Pannu786 <thepannu786@gmail.com>
2022-04-05 12:50:19 +0530

    added category in Data cmp manually + base setup ready for CreatePin cmp

--- src/components/CreatePin.js
+++ src/components/CreatePin.js
@@ -1,8 +1,25 @@
+import useState from 'react';
+import { AiOutlineCloudUpload } from 'react-icons/ai';
+import { MdDelete } from 'react-icons/md';
+import { useNavigate } from 'react-router-dom';
 
-const CreatePin = () => {
-  return (
-    <div>CreatePin</div>
-  )
-}
+import { client } from '../client';
+import Spinner from './Spinner';
+import { categories } from '../utils/data';
 
-export default CreatePin
+const CreatePin = ({ user }) => {
+  const [title, setTitle] = useState('');
+  const [about, setAbout] = useState('');
+  const [Destination, setDestination] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [fields, setFields] = useState(null);
+  const [category, setCategory] = useState(null);
+  const [imageAsset, setImageAsset] = useState(null);
+  const [wrongImageType, setWrongImageType] = useState(false);
+
+  const navigate = useNavigate();
+
+  return <div>CreatePin</div>;
+};
+
+export default CreatePin;
