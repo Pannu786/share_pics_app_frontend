43668d525d937347941809401dc23f778607837e
Pannu786 <thepannu786@gmail.com>
2022-04-06 09:27:22 +0530

    added fecting logic into PinDetail cmp

--- src/components/PinDetail.js
+++ src/components/PinDetail.js
@@ -1,8 +1,42 @@
+import { useState, useEffect } from 'react';
+import { MdDownloadForOffline } from 'react-icons/md';
+import { Link, useParams } from 'react-router-dom';
+import { v4 as uuidv4 } from 'uuid';
 
-const PinDetail = () => {
-  return (
-    <div>PinDetail</div>
-  )
-}
+import { client, urlFor } from '../client';
+import MasonryLayout from './MasonryLayout';
+import { pinDetailMorePinQuery, pinDetailQuery } from '../utils/data';
+import Spinner from './Spinner';
 
-export default PinDetail
+const PinDetail = ({ user }) => {
+  const [pins, setPins] = useState(null);
+  const [pinDetail, setPinDetail] = useState(null);
+  const [comment, setComment] = useState('');
+  const [addingComment, setAddingComment] = useState(false);
+
+  const { pinId } = useParams();
+
+  if (!pinDetail) return <Spinner message='Loading pin details...' />;
+
+
+  //* fetching Pin details from the server (Sanity Backend)
+  const fetchPinDetails = () => {
+    let query = pinDetailQuery(pinId);
+
+    if (query) {
+      client.fetch(query).then((data) => {
+        setPinDetail(data[0]);
+
+        if (data[0]) {
+          query = pinDetailMorePinQuery(data[0]);
+
+          client.fetch(query).then((res) => setPins(res));
+        }
+      });
+    }
+  };
+
+  return <div>PinDetail</div>;
+};
+
+export default PinDetail;
